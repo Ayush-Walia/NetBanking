e4222e483e535fb282ab3f2a9e81d20574d5195c
Poison-Fang <ayushwalia999@gmail.com>
2019-04-04 11:31:31 +0530

    fix edit button fetching info againg

--- website/bank/assets/js/settings.js
+++ website/bank/assets/js/settings.js
@@ -4,6 +4,11 @@ if(document.getElementById("settingsPage")!=null){
 getUserInfo();
 }
 
+var nameEditable = true;
+var contactEditable = true;
+var passwordEditable = true;
+var addressEditable = true;
+
 function getUserInfo(){	
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
@@ -25,26 +30,36 @@ xmlhttp.send(JSON.stringify(userId));
 }
 
 function editName(){
+    if(nameEditable==true){
     var nameValue = document.getElementById("name").innerHTML;
     var genderValue = document.getElementById("gender").innerHTML;
     var dobValue = document.getElementById("dob").innerHTML;
     document.getElementById("name").innerHTML="<input type='text' value='"+nameValue+"'></span><span class='bar'></span>";
     document.getElementById("gender").innerHTML="<input type='text' value='"+genderValue+"'></span><span class='bar'></span>";
-    document.getElementById("dob").innerHTML="<input type='text' value='"+dobValue+"'></span><span class='bar'></span>";    
+    document.getElementById("dob").innerHTML="<input type='text' value='"+dobValue+"'></span><span class='bar'></span>"; 
+    nameEditable = false;
+    }
 }
 
 function editContact(){
+    if(contactEditable==true){
     var phoneNumberValue = document.getElementById("phoneNumber").innerHTML;
     document.getElementById("phoneNumber").innerHTML="<input type='text' value='"+phoneNumberValue+"'></span><span class='bar'></span>";
+    contactEditable = false;    
+    }
 }
 
 function editPassword(){
+    if(passwordEditable==true){
     document.getElementById("userPassword").innerHTML="<input placeholder='Type old Password' type='text' value=''></span><span class='bar'></span>";
     document.getElementById("newPassword").innerHTML="<input placeholder='Type new Password' type='text' value=''></span><span class='bar'></span>";
     document.getElementById("newPasswordConfirm").innerHTML="<input placeholder='Confirm new Password' type='text' value=''></span><span class='bar'></span>";
+    passwordEditable = false;    
+    }
 }
 
 function editAddress(){
+    if(addressEditable==true){
     var userStreetValue = document.getElementById("userStreet").innerHTML;
     var userCityValue = document.getElementById("userCity").innerHTML;
     var userStateValue = document.getElementById("userState").innerHTML;
@@ -52,5 +67,7 @@ function editAddress(){
     document.getElementById("userCity").innerHTML="<input type='text' value='"+userCityValue+"'></span><span class='bar'></span>";
     if(userStateValue.length>0){
     document.getElementById("userState").innerHTML="<input type='text' value='"+userStateValue+"'></span><span class='bar'></span>";
+    addressEditable = false;
+        }
     }
 }
